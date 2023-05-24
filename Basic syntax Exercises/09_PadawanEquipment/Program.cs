cdbf96d45a0b57b3f3889b6a4e868101f88a6b91
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-05-25 00:04:02 +0300

    9 and 10 ex

---
+++ Basic syntax Exercises/09_PadawanEquipment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.Reflection.Emit;
+
+namespace _09_PadawanEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            float johnMoney = float.Parse(Console.ReadLine());  
+            int studentCount = int.Parse(Console.ReadLine());
+            float lightSaberPrice = float.Parse(Console.ReadLine());
+            float robePrice = float.Parse(Console.ReadLine());
+            float beltPrice = float.Parse(Console.ReadLine());
+
+
+            int lightSaberCount = 0;
+            int beltCount = studentCount;
+            float totalSum = 0;
+            float change = 0;
+
+
+            lightSaberCount = (int)Math.Ceiling(studentCount + (studentCount * 0.1));
+           
+            if (studentCount >= 6) 
+            {
+                beltCount  = studentCount - (studentCount/6);
+            }
+
+            totalSum = (lightSaberCount * lightSaberPrice) + (beltCount * beltPrice) + studentCount * robePrice;
+
+            if (totalSum > johnMoney)
+            {
+                change =  totalSum - johnMoney;
+                Console.WriteLine($"John will need {change:F2}lv more.");
+            }
+            else
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalSum:F2}lv.");
+            }
+
+          
+
+        }
+    }
+}
---
+++ Basic syntax Exercises/10_RageExpenses/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _10_RageExpenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int lostGames = int.Parse(Console.ReadLine());  
+            float headsetPrice = float.Parse(Console.ReadLine());
+            float mousePrice = float.Parse(Console.ReadLine());
+            float keyboardPrice = float.Parse(Console.ReadLine());
+            float displayPrice = float.Parse(Console.ReadLine());
+
+
+            int trashedHeadsets = 0;
+            int trashedMice = 0;
+            int trashedKeybords = 0;
+            int trashedDisplayss = 0;
+            float totalSum = 0;
+
+            for(int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                  
+                    trashedKeybords ++;
+                    if (trashedKeybords % 2 == 0)
+                    {
+                        trashedDisplayss ++;
+                    }
+                    
+                }
+                if (i % 2 == 0)
+                {
+                    trashedHeadsets ++;
+                }
+                if(i % 3 == 0)
+                {
+                    trashedMice ++;
+                }
+                
+            }
+
+            totalSum = (trashedHeadsets * headsetPrice) + (trashedKeybords * keyboardPrice) + (trashedDisplayss * displayPrice) + (trashedMice * mousePrice);
+
+            Console.WriteLine($"Rage expenses: {totalSum:F2} lv.");
+        }
+    }
+}
