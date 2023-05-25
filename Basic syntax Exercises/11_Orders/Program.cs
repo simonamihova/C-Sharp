009114b1ec124b338b72f3a00ba5aae9c4267010
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-05-25 13:02:09 +0300

    ex 11

---
+++ Basic syntax Exercises/11_Orders/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _11_Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int N = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+
+            for (int i = 0; i<N; i++)
+            {
+                double capsulePrice =  double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());   
+                int capsuleCount = int.Parse(Console.ReadLine());
+
+                double priceForOrder = (days * capsuleCount) * capsulePrice;
+
+                Console.WriteLine($"The price for the coffee is: ${priceForOrder:F2}");
+
+                totalPrice += priceForOrder;
+
+            }
+            
+        
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+
+
+        }
+    }
+}
