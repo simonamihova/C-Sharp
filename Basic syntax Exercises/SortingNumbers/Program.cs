112eef9c1dc2da9060d5c3f91f7ddb35dc91ee3e
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-05-30 20:31:34 +0300

    new changes

---
+++ Basic syntax Exercises/SortingNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SortingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Create a program that receives three real numbers and 
+             sorts them in descending order. Print each number on a new line.
+            */
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
