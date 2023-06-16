3a4c845b8ed1db3ce42d756627e0b3aee947b6f9
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-06-16 13:45:04 +0300

    math power

---
+++ MethodsLab/08_MathPower/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08_MathPower
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Input
+            int baseNum = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int result = MathPower(baseNum, power);
+            Console.WriteLine(result);
+        }
+
+        static int MathPower(int baseNum, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= baseNum;
+            }
+            return result;
+        }
+    }
+}
