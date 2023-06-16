affe3ecf838323d31ca24754c6fd1992ee6b91a5
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-06-16 13:33:13 +0300

    repeat string method

---
+++ MethodsLab/07_RepeatString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07_RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string inputString = Console.ReadLine();
+           int repeatCount = int.Parse(Console.ReadLine());
+            string result = RepeatString(inputString, repeatCount);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString(string inputString, int repeatCount) 
+        {
+            string finalString = "";
+            for (int i = 0; i < repeatCount; i++)
+            {
+                finalString += inputString;
+            }
+            return finalString;
+        }
+    }
+}
