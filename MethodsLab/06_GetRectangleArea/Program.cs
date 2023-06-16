eb83437b863877efc5eb521ecd958c429d9e4067
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-06-16 13:26:08 +0300

    rectangle area method

---
+++ MethodsLab/06_GetRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _06_GetRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height =  int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int area = GetRectangleArea(height, width);
+            Console.WriteLine(area);
+
+        }
+        static int GetRectangleArea(int height, int width)
+        {
+            int area = height * width;
+            return area;
+        } 
+    }
+}
