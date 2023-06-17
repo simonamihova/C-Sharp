ef9f0ce3cdf3f6eb635b374da124d305e5235240
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-06-17 10:36:12 +0300

    methods lab last

---
+++ MethodsLab/10_MultiplyEvenByOdds/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _10_MultiplyEvenByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                 .Split("")
+                                 .Select(int.Parse)
+                                 .ToArray();
+            for (int i = 0;i < input.Length; i++)
+            {
+                input[i] = Math.Abs(input[i]);
+            }
+
+            int result = GetMultipleOfEvenAndOdds(input);
+            Console.WriteLine(result);
+        }
+        static int GetSumOfOddDigits(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]%2 != 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
+        static int GetSumOfEvenDigits(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    sum += arr[i];
+                }
+            }
+            return sum;
+        }
+        static int GetMultipleOfEvenAndOdds(int[] arr)
+        {
+            int result = 0;
+            int evens = GetSumOfEvenDigits(arr);
+            int odds = GetSumOfOddDigits(arr);
+            result = evens * odds;
+            return result;
+        }
+
+    }
+   
+}
---
+++ MethodsLab/11_MathOperations/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _11_MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Write a method that receives two numbers and an operator, calculates the result and returns it. You will be given
+                three lines of input. The first will be the first number, the second one will be the operator and the last one will be
+                the second number.
+                The possible operators are: /, *, + and -.
+             */
+
+            int fNumber = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine()); 
+            int lNumber  = int.Parse(Console.ReadLine());
+
+            double finalResult = 0;
+
+            switch(operation)
+            {
+                case '/':
+                    finalResult = division(fNumber, operation, lNumber);
+                    break;
+                case '*':
+                    finalResult = multiplication(fNumber, operation, lNumber);
+                    break;
+                case '+':
+                    finalResult = sum(fNumber, operation, lNumber);
+                    break;
+                case '-':   
+                    finalResult = substarction(fNumber, operation, lNumber);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operation");
+                    break;
+            }
+            Console.WriteLine(finalResult.ToString());
+
+        }
+        //The possible operators are: /, *, + and -.
+
+        static double division (int a, char operation, int b)
+        {
+            double result = a / b;
+            return result;
+        }
+        static double multiplication(int a, char operation, int b)
+        {
+            double result = a * b;
+            return result;
+        }
+        static double sum(int a, char operation, int b)
+        {
+            double result = a + b;
+            return result;
+        }
+        static double substarction(int a, char operation, int b)
+        {
+            double result = a - b;
+            return result;
+        }
+    }
+}
