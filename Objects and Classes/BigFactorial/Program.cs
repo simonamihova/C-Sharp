311b2da1fe1bb1575af3d4331474ee8064fa319d
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-06-28 14:35:42 +0300

    ex1 ad 2

---
+++ Objects and Classes/BigFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+namespace BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            BigInteger factorial = 1;
+            
+            for (int i = 2; i <= input; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
---
+++ Objects and Classes/RandomizeWords/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                                  .Split(" ")
+                                  .ToArray();
+
+            Random rnd = new Random();
+
+            for (int i  = 0; i < input.Length; i++)
+            {
+                int randomIndex = rnd.Next(0, input.Length);
+                string currentEl = input[i];
+                string randomEl = input[randomIndex];
+
+                input[i] = randomEl;
+                input[randomIndex] = currentEl;
+            }
+
+            foreach (string element in input)
+            {
+                Console.WriteLine(element);
+            }
+            
+        }
+    }
+}
