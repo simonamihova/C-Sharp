35c4a87646cb35dea808b0e4d6a50bd4bcc0fa61
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2023-10-09 17:38:26 +0300

    advanced first topic

---
+++ Advanced/Stacks and Queues/ReverseString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var str in input)
+            {
+                stack.Push(str);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Advanced/Stacks and Queues/StackSum/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace StackSum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine()
+                                                     .Split(' ')
+                                                     .Select(int.Parse)); 
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] commandArr = command.Split(' ');
+
+                if (commandArr.Contains("add"))
+                {
+                    int first = int.Parse(commandArr[1]);
+                    int second = int.Parse(commandArr[2]);
+                    stack.Push(first);
+                    stack.Push(second); 
+
+                }
+                else if (commandArr.Contains("remove"))
+                {
+                    int n = int.Parse(commandArr[1]);
+                    if (stack.Count >= n)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            int sum = 0;
+
+            if (stack.Count > 0)
+            {
+                sum += stack.Pop();
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
