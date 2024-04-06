37c11bb70154e552c3f12dbde9b486c7111ef41d
Mihovaa <93177847+Mihovaa@users.noreply.github.com>
2024-04-06 22:58:33 +0300

    js

--- OOP/Inheritance/Inheritance/01_Person/Program.cs
+++ OOP/Inheritance/Inheritance/01_Person/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace _01_Person
+﻿using System.Text;
+
+namespace _01_Person
 {
     internal class Program
     {
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public Person(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Name: {this.Name}");
+                sb.AppendLine($"Age: {this.Age}");
+                return sb.ToString();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
